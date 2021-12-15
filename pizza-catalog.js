26e6027a684f9190913125ebfd82be0c25768dd8
DeanPentaghast <81925859+DeanPentaghast@users.noreply.github.com>
2021-12-15 21:24:34 +0100

    Add files via upload

---
+++ pizza-catalog.js
@@ -0,0 +1,486 @@
+const pizza_1 = {
+    name: "MARGARITA",
+    ingredients: ["Tomate", "Queso", "Oregano"],
+    isVegetarian: true,
+    almostVeg: false,
+    num: "01"
+}
+
+const pizza_2 = {
+    name: "SALAMI",
+    ingredients: ["Tomate", "Queso", "Salami", "Oregano"],
+    isVegetarian: false,
+    almostVeg: true,
+    num: "02"
+}
+
+const pizza_3 = {
+    name: "BACON",
+    ingredients: ["Tomate", "Queso", "Bacon", "Oregano"],
+    isVegetarian: false,
+    almostVeg: true,
+    num: "03"
+}
+
+const pizza_4 = {
+    name: "JAMON",
+    ingredients: ["Tomate", "Queso", "Jamon York", "Oregano"],
+    isVegetarian: false,
+    almostVeg: true,
+    num: "04"
+}
+
+const pizza_5 = {
+    name: "PEPPERONI",
+    ingredients: ["Tomate", "Queso", "Pepperoni", "Oregano"],
+    isVegetarian: false,
+    almostVeg: true,
+    num: "05"
+}
+
+const pizza_6 = {
+    name: "NAPOLI",
+    ingredients: ["Tomate", "Queso", "Atun", "Bacon", "Jamon", "Huevo", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "06"
+}
+
+const pizza_7 = {
+    name: "BOLYWOOD",
+    ingredients: ["Tomate", "Queso", "Jamon York", "Bacon", "Atun", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "07"
+}
+
+const pizza_8 = {
+    name: "CARMEN",
+    ingredients: ["Tomate", "Queso", "Jamon York", "Salami", "Champiñon", "Cebolla", "Huevo", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "07A"
+}
+
+const pizza_9 = {
+    name: "ROMANA",
+    ingredients: ["Tomate", "Queso", "Jamon", "Champiñon", "Oregano"],
+    isVegetarian: false,
+    almostVeg: true,
+    num: "08"
+}
+
+const pizza_10 = {
+    name: "ANGEL",
+    ingredients: ["Tomate", "Queso", "Bacon", "Jamon York", "Atun", "Champiñones", "Olivas", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "09"
+}
+
+const pizza_11 = {
+    name: "SUPER",
+    ingredients: ["Tomate", "Queso", "Jamon York", "Champiñon", "Cebolla", "Pepperoni", "Aceitunas", "Anchoas", "Huevo", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "09A"
+}
+
+const pizza_12 = {
+    name: "BANDITO",
+    ingredients: ["Tomate", "Queso", "Jamon", "Bacon", "Pollo", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "10"
+}
+
+const pizza_13 = {
+    name: "FUNGI",
+    ingredients: ["Tomate", "Queso", "Jamon", "Atun", "Salami", "Champiñon", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "10A"
+}
+
+const pizza_14 = {
+    name: "4 ESTACIONES",
+    ingredients: ["Tomate", "Queso", "Jamon York", "Salami", "Champiñon", "Pimiento", "Huevo", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "11"
+}
+
+const pizza_15 = {
+    name: "CARBONARA I",
+    ingredients: ["Bacon", "Cebolla", "Queso", "Nata"],
+    isVegetarian: false,
+    almostVeg: true,
+    num: "12"
+}
+
+const pizza_16 = {
+    name: "CARBONARA II",
+    ingredients: ["Bacon", "Champiñon", "Queso", "Nata", "Huevo"],
+    isVegetarian: false,
+    almostVeg: true,
+    num: "12A"
+}
+
+const pizza_17 = {
+    name: "4 QUESOS",
+    ingredients: ["Tomate", "Queso", "Oregano"],
+    isVegetarian: true,
+    almostVeg: false,
+    num: "13"
+}
+
+const pizza_18 = {
+    name: "ATUN",
+    ingredients: ["Tomate", "Queso", "Atun", "Cebolla", "Ajo", "Oregano"],
+    isVegetarian: false,
+    almostVeg: true,
+    num: "14"
+}
+
+const pizza_19 = {
+    name: "GAMBERINI",
+    ingredients: ["Tomate", "Queso", "Gambas", "Ajo", "Cebolla", "Oregano"],
+    isVegetarian: false,
+    almostVeg: true,
+    num: "15"
+}
+
+const pizza_20 = {
+    name: "MAFIA",
+    ingredients: ["Tomate", "Queso", "Jamon", "Salami", "Champiñon", "Guindilla", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "15A"
+}
+
+const pizza_21 = {
+    name: "POLLO",
+    ingredients: ["Tomate", "Queso", "Pollo", "Cebolla", "Huevo", "Oregano"],
+    isVegetarian: false,
+    almostVeg: true,
+    num: "16"
+}
+
+const pizza_22 = {
+    name: "IBERICA",
+    ingredients: ["Tomate", "Queso", "Jamon Serrano", "Tomate Fresco"],
+    isVegetarian: false,
+    almostVeg: true,
+    num: "17"
+}
+
+const pizza_23 = {
+    name: "VEGETAL ESPECIAL",
+    ingredients: ["Tomate", "Queso", "Pimiento", "Champiñon", "Cebolla", "Alcachofa", "Olivas", "Oregano"],
+    isVegetarian: true,
+    almostVeg: false,
+    num: "18"
+}
+
+const pizza_24 = {
+    name: "AL HUEVO",
+    ingredients: ["Tomate", "Queso", "Jamon York", "Huevo", "Oregano"],
+    isVegetarian: false,
+    almostVeg: true,
+    num: "19"
+}
+
+const pizza_25 = {
+    name: "PIZZA HOT",
+    ingredients: ["Tomate", "Queso", "Chorizo", "Bacon", "Jamon York", "Cebolla", "Tabasco", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "20"
+}
+
+const pizza_26 = {
+    name: "HAWAIANA",
+    ingredients: ["Tomate", "Queso", "Jamon York", "Piña", "Oregano"],
+    isVegetarian: false,
+    almostVeg: true,
+    num: "21"
+}
+
+const pizza_27 = {
+    name: "VALENCIANA",
+    ingredients: ["Tomate", "Queso", "Champiñon", "Gambas", "Pollo", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "22"
+}
+
+const pizza_28 = {
+    name: "CHICAGO",
+    ingredients: ["Tomate", "Queso", "Frankfurt", "Patatas Fritas", "Huevo", "Oregano"],
+    isVegetarian: false,
+    almostVeg: true,
+    num: "23"
+}
+
+const pizza_29 = {
+    name: "PIZZA PICA",
+    ingredients: ["Tomate", "Queso", "Jamon York", "Salami", "Chorizo Picante", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "23A"
+}
+
+const pizza_30 = {
+    name: "PUNJAB",
+    ingredients: ["Tomate", "Queso", "Pollo", "Pimiento", "Champiñon", "Cebolla", "Ajo", "Oregano"],
+    isVegetarian: false,
+    almostVeg: true,
+    num: "24"
+}
+
+const pizza_31 = {
+    name: "BURGOS",
+    ingredients: ["Tomate", "Queso", "Jamon York", "Bacon", "Pepperoni", "Olivas Negras", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "25"
+}
+
+const pizza_32 = {
+    name: "AMERICANA",
+    ingredients: ["Tomate", "Queso", "Atun", "Pollo", "Cebolla", "Pimiento", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "26"
+}
+
+const pizza_33 = {
+    name: "DONER PIZZA",
+    ingredients: ["Tomate", "Queso", "Bacon", "Champiñon", "Carne Asada", "Salsa", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "26A"
+}
+
+const pizza_34 = {
+    name: "ANCHOAS",
+    ingredients: ["Tomate", "Queso", "Tomate Fresco", "Anchoas", "Olivas Negras", "Oregano"],
+    isVegetarian: false,
+    almostVeg: true,
+    num: "27"
+}
+
+const pizza_35 = {
+    name: "ITALIA",
+    ingredients: ["Tomate", "Queso", "Bacon", "Champiñon", "Jamon York", "Gambas", "Ajo", "Oregnao"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "28"
+}
+
+const pizza_36 = {
+    name: "NEW YORK",
+    ingredients: ["Tomate", "Queso", "Jamon York", "Pimiento", "Champiñon", "Alcachofa", "Aceitunas", "Oregano"],
+    isVegetarian: false,
+    almostVeg: true,
+    num: "29"
+}
+
+const pizza_37 = {
+    name: "PINOCHO",
+    ingredients: ["Tomate", "Queso", "Pollo", "Jamon York", "Champiñon", "Huevo" ,"Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "30"
+}
+
+const pizza_38 = {
+    name: "CALZONE",
+    ingredients: ["Tomate", "Queso", "Jamon", "Salami", "Champiñon", "Atun", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "31"
+}
+
+const pizza_39 = {
+    name: "BOLOÑESA",
+    ingredients: ["Tomate", "Queso", "Salsa Boloñesa", "Jamon", "Bacon", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "32"
+}
+
+const pizza_40 = {
+    name: "MIRAMARE",
+    ingredients: ["Tomate", "Queso", "Jamon", "Pepperoni", "Salami", "Champiñon", "Cebolla", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "33"
+}
+
+const pizza_41 = {
+    name: "BARBACOA I",
+    ingredients: ["Queso", "Bacon", "Jamon York", "Patatas Fritas", "Salsa Barbacoa", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "34"
+}
+
+const pizza_42 = {
+    name: "BARBACOA II",
+    ingredients: ["Pollo", "Queso", "Patatas Fritas", "Cebolla", "Salsa Barbacoa", "Oregano"],
+    isVegetarian: false,
+    almostVeg: true,
+    num: "35"
+}
+
+const pizza_43 = {
+    name: "BARBACOA III",
+    ingredients: ["Queso", "Jamon York", "Bacon", "Carne Picada", "Salsa Barbacoa", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "36"
+}
+
+const pizza_44 = {
+    name: "BARBACOA IV",
+    ingredients: ["Salsa Barbacoa", "Queso", "Jamon York", "Pollo", "Bacon", "Champiñon", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "36A"
+}
+
+const pizza_45 = {
+    name: "STAR",
+    ingredients: ["Tomate", "Queso", "Jamon York", "Atun", "Champiñon", "Pimiento", "Cebolla", "Aceitunas", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "37"
+}
+
+const pizza_46 = {
+    name: "PIZZA KEBAB MIXTA",
+    ingredients: ["Tomate", "Queso", "Ternera", "Pollo", "Bacon", "Jamon", "Oregano"],
+    isVegetarian: false,
+    almostVeg: false,
+    num: "37A"
+}
+
+let txt = "<h2>PIZZAS:</h2>";
+
+let numPizzas = 47; // numero de pizzas + 1 para el loop
+
+// for (var j = 0; j < pizza_2.ingredients.length ; j++) {
+//     document.getElementById("prueba").innerHTML +=
+//     pizza_2.ingredients[j];
+// };
+
+// |                                                                              |
+// V esta funcion sirve para sacar arrays de todos los ingredientes de las pizzas V
+
+// function loopIngredients(ingArray) {
+//     for (var i = 0; i < ingArray.length; i++) {
+//         console.log(ingArray[i]);
+//     }
+// }
+
+function replaceInArray(str) {
+    return str.replace(/\s/g, '-');
+};
+
+function hideShow(myClass, visible) {
+    [].forEach.call(document.getElementsByClassName(myClass), function (el) { el.hidden = !visible;    });
+}
+
+
+// function deselectAll () {
+//     var ele = document.getElementsByClassName("ingredientCheckbox");
+//     for (var i = 0; i < ele.length; i++) {
+//         ele[i].checked = false;
+//     }
+//     console.log("deselecciona");
+// };
+
+// for (var j = 1; j < numPizzas; j++) {
+//     if (eval("pizza_" + j).isVegetarian == true) {
+//         loopIngredients(eval("pizza_" + j).ingredients);
+//     } else {
+//         console.log("no es vegeta");
+//     };
+// };
+
+// for (var i = 1; i<47; i++) {
+//     console.log(i);
+//     for (var j =0; j < eval("pizza_" + i); j++ ) {
+//         document.getElementById("prueba").innerHTML += eval("pizza_" + i).ingredients[j]
+//     };
+// };
+
+for (var i = 1; i<47; i++) {
+    if (eval("pizza_" + i).isVegetarian == true) {
+        txt += 
+    "<p class=\"vegeta " + eval("pizza_" + i).ingredients.map(replaceInArray).join(" ") + "\" id=\"pizza-" + i + "\" hidden><strong>" + eval("pizza_" + i).num + ": " + eval("pizza_" + i).name + ": </strong>" + eval("pizza_" + i).ingredients.join(", ") + "</p>"
+    } else if (eval("pizza_" + i).almostVeg == true) {
+        txt += 
+    "<p class=\"casiVegeta " + eval("pizza_" + i).ingredients.map(replaceInArray).join(" ") + "\" id=\"pizza-" + i + "\" hidden><strong>"+ eval("pizza_" + i).num + ": " + eval("pizza_" + i).name + ": </strong>" + eval("pizza_" + i).ingredients.join(", ") + "</p>"
+    } else {
+        txt += 
+    "<p class=\"" + eval("pizza_" + i).ingredients.map(replaceInArray).join(" ") + "\" id=\"pizza-" + i + "\" hidden><strong>"+ eval("pizza_" + i).num + ": " + eval("pizza_" + i).name + ": </strong>" + eval("pizza_" + i).ingredients.join(", ") + "</p>"
+    }
+
+};
+
+// for (var i = 1; i<47; i++) {
+//     txt += 
+//     "<p class=\"" + eval("pizza_" + i).ingredients.map(replaceInArray).join(" ") + "\" id=\"pizza-" + i + "\">"+ eval("pizza_" + i).num + ": " + eval("pizza_" + i).name + ": " + eval("pizza_" + i).ingredients.join(", ") + "</p>"
+//     + "</p>"
+
+// };
+
+document.getElementById("pizzas-container").innerHTML = txt;
+
+document.getElementById('queso').onchange = function() { hideShow ('Queso', this.checked); }
+document.getElementById('tomate').onchange = function() { hideShow ('Tomate', this.checked); }
+document.getElementById('oregano').onchange = function() { hideShow ('Oregano', this.checked); }
+document.getElementById('salami').onchange = function() { hideShow ('Salami', this.checked); }
+document.getElementById('bacon').onchange = function() { hideShow ('Bacon', this.checked); }
+document.getElementById('jamon-york').onchange = function() { hideShow ('Jamon-York', this.checked); }
+document.getElementById('pepperoni').onchange = function() { hideShow ('Pepperoni', this.checked); }
+document.getElementById('atun').onchange = function() { hideShow ('Atun', this.checked); }
+document.getElementById('jamon').onchange = function() { hideShow ('Jamon', this.checked); }
+document.getElementById('huevo').onchange = function() { hideShow ('Huevo', this.checked); }
+document.getElementById('champinon').onchange = function() { hideShow ('Champiñon', this.checked); }
+document.getElementById('cebolla').onchange = function() { hideShow ('Cebolla', this.checked); }
+document.getElementById('olivas').onchange = function() { hideShow ('Olivas', this.checked); }
+document.getElementById('aceitunas').onchange = function() { hideShow ('Aceitunas', this.checked); }
+document.getElementById('anchoas').onchange = function() { hideShow ('Anchoas', this.checked); }
+document.getElementById('pollo').onchange = function() { hideShow ('Pollo', this.checked); }
+document.getElementById('gambas').onchange = function() { hideShow ('Gambas', this.checked); }
+document.getElementById('ajo').onchange = function() { hideShow ('Ajo', this.checked); }
+document.getElementById('guindilla').onchange = function() { hideShow ('Guindilla', this.checked); }
+document.getElementById('jamon-serrano').onchange = function() { hideShow ('Jamon-Serrano', this.checked); }
+document.getElementById('tomate-fresco').onchange = function() { hideShow ('Tomate-Fresco', this.checked); }
+document.getElementById('pimiento').onchange = function() { hideShow ('Pimiento', this.checked); }
+document.getElementById('alcachofa').onchange = function() { hideShow ('Alcachofa', this.checked); }
+document.getElementById('chorizo').onchange = function() { hideShow ('Chorizo', this.checked); }
+document.getElementById('tabasco').onchange = function() { hideShow ('Tabasco', this.checked); }
+document.getElementById('pina').onchange = function() { hideShow ('Piña', this.checked); }
+document.getElementById('frankfurt').onchange = function() { hideShow ('Frankfurt', this.checked); }
+document.getElementById('patatas-fritas').onchange = function() { hideShow ('Patatas-Fritas', this.checked); }
+document.getElementById('chorizo-picante').onchange = function() { hideShow ('Chorizo-Picante', this.checked); }
+document.getElementById('olivas-negras').onchange = function() { hideShow ('Olivas-Negras', this.checked); }
+document.getElementById('carne-asada').onchange = function() { hideShow ('Carne-Asada', this.checked); }
+document.getElementById('salsa-bolonesa').onchange = function() { hideShow ('Salsa-Boloñesa', this.checked); }
+document.getElementById('salsa-barbacoa').onchange = function() { hideShow ('Salsa-Barbacoa', this.checked); }
+document.getElementById('carne-picada').onchange = function() { hideShow ('Carne-Picada', this.checked); }
+document.getElementById('ternera').onchange = function() { hideShow ('Ternera', this.checked); }
+
+document.getElementById('vegeta').onchange = function() { hideShow ('vegeta', this.checked); }
+document.getElementById('casi-vegeta').onchange = function() { hideShow ('casiVegeta', this.checked); }
+
+//document.getElementById("pizzas").innerHTML = "caca";
+
+//for (var i = 1; i<47; i++) {
+//    var print = "pizza_" + i;
+//    console.log(eval(print));
+//};
